48ff15780af7fd87d032a7bbc1b09b344a843081
semyon-puchin <s.puchin@hashcib.com>
2022-07-04 11:32:39 +0500

    Redeclared entities to describe PLACEMENT

---
+++ orm/models/hubs/h_pools.py
@@ -0,0 +1,18 @@
+from enum import Enum
+from sqlalchemy import Column, Integer, Text, Boolean, DateTime
+
+from orm.consts.revenue_type import RevenueType
+from main import Base
+
+
+class HubPools(Base):
+
+    __tablename__ = 'h_pools'
+
+    h_pool_id = Column(Integer, primary_key=True)
+    # ---
+    h_pool_label = Column(Text, nullable=False)
+    h_revenue_type = Column(Enum(RevenueType), nullable=False)
+    h_is_impermanent_loss = Column(Boolean, nullable=False)
+
+    h_pool_load_ts = Column(DateTime, nullable=False)
---
+++ orm/models/links/l_chains_protocols_pools_addresses.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, ForeignKey
+
+from main import Base
+
+
+class LinkChainsProtocolsPoolsAddresses(Base):
+
+    __tablename__ = 'l_chains_protocols_pools_addresses'
+
+    l_chain_protocol_pool_address_id = Column(Integer, primary_key=True)
+    # ---
+    l_address_chain_id = Column(Integer, ForeignKey('l_addresses_chains.l_address_chain_id'), nullable=False)
+    l_address_pool_id = Column(Integer, ForeignKey('l_addresses_pools.l_address_pool_id'), nullable=False)
+    l_chain_protocol_id = Column(Integer, ForeignKey('l_chains_protocols.l_chain_protocol_id'), nullable=False)
